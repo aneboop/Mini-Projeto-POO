779676b7ff3debdac0be34cf3508b35eb33dad4c
Mari <mmariane691@gmail.com>
2025-07-18 19:44:24 -0300

    Criando classe de produtos

---
+++ loja/model/produto/Produto.java
@@ -0,0 +1,12 @@
+import java.math.BigDecimal;
+
+public class Produto {
+    private String nome;
+    private String codigo;
+    private int estoque;
+    private BigDecimal precoBase;
+
+    public Produto(String nome, String codigo, int estoque, BigDecimal precoBase){
+        
+    }
+}
