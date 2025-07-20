262e05d507266a80444706efd6fd3d779eb808d4
joaolucasaxe <jlmachado1406@gmail.com>
2025-07-20 11:36:26 -0300

    corecao menu(2 a 4)

--- loja/model/UI/ConsoleMenu.java
+++ loja/model/UI/ConsoleMenu.java
@@ -10,6 +10,7 @@ import loja.model.produto.ProdutoDigital;
 import loja.model.produto.ProdutoFisico;
 
 public class ConsoleMenu {
+
     public static void exibirMenuPrincipal() {
         System.out.println("\n------ Menu Principal ------");
         System.out.println("1. Cadastrar Produto");
@@ -31,7 +32,7 @@ public class ConsoleMenu {
         int estoque = InputUtils.getInput("Estoque inicial: ");
         BigDecimal precoBase = InputUtils.getBigDecimalInput("Preço base: ");
        
-
+      
         System.out.println("Tipo de Produto:");
         System.out.println("1. Produto Físico");
         System.out.println("2. Produto Digital");
@@ -50,6 +51,78 @@ public class ConsoleMenu {
         }
 
     }
+    //OPCAO 2
+    public static void alterarProduto(Produto produtoalterado){
+         System.out.println("\n------ ALTERACAO DE PRODUTO ------");
+         System.out.println("Escolha qual atributo deseja alrerar"); 
+         System.out.println("1. Preco Base"); 
+         System.out.println("2. Estoque"); 
+         int tipoAlteracao = InputUtils.getInput("Escolha uma opcao: ");
+
+         if(tipoAlteracao == 1){
+            BigDecimal precoBase = InputUtils.getBigDecimalInput("Novo preço base: ");
+            produtoalterado.setPrecoBase(precoBase);
+         }else if(tipoAlteracao == 2){
+            int estoque = InputUtils.getInput("Novo estoque: ");
+            produtoalterado.setEstoque(estoque);
+         }else {
+            System.out.println("Opcao inválida.");
+            
+        }
+    }
+    //OPCAO 3
+    public static Cliente cadastrarCliente(){
+        System.out.println("\n------ CADASTRO DE CLIENTE ------");
+        String nome = InputUtils.getStringInput("Nome: ");
+        String endereco = InputUtils.getStringInput("Endereço: ");
+        String telefone = InputUtils.getStringInput("Telefone: ");
+        String identificadorBase = InputUtils.getStringInput("Codigo: ");
+        String digitoIdentificador;
+        String indentifucadorFinal;
+
+        System.out.println("Tipo de Cliente:");
+        System.out.println("1. Pessoa Física");
+        System.out.println("2. Pessoa Juridica");
+        int tipoCliente = InputUtils.getInput("Escolha uma opcao: ");
+
+        if(tipoCliente == 1){
+            digitoIdentificador = "01-";
+            indentifucadorFinal = digitoIdentificador+identificadorBase;
+            String CPF = InputUtils.getStringInput("CPF: ");
+            return new PessoaFisica(indentifucadorFinal, nome, endereco, telefone, CPF);
+        } else if (tipoCliente == 2) {
+            digitoIdentificador = "02-";
+            indentifucadorFinal = digitoIdentificador+identificadorBase;
+            String CNPJ = InputUtils.getStringInput("CNPJ: ");
+            return new PessoaJuridica(indentifucadorFinal, nome, endereco, telefone, CNPJ);
+        }else {
+            System.out.println("Tipo de produto inválido.");
+            return null;
+        }
+
+
+    }
+    //OPCAO 4
+    public static void alterarCliente(Cliente clientealterado){
+         System.out.println("\n------ ALTERACAO DE CLIENTE ------");
+         System.out.println("Escolha qual atributo deseja alrerar"); 
+         System.out.println("1. Telefone"); 
+         System.out.println("2. Endereco"); 
+         int tipoAlteracao = InputUtils.getInput("Escolha uma opcao: ");
+
+         if(tipoAlteracao == 1){
+            String telefone = InputUtils.getStringInput("Novo telefone base: ");
+            clientealterado.setTelefone(telefone);
+         }else if(tipoAlteracao == 2){
+            String endereco = InputUtils.getStringInput("Novo endereco base: ");
+            clientealterado.setTelefone(endereco);
+         }else {
+            System.out.println("Opcao inválida.");
+            
+        }
+    }
+    //OPCAO 5
+    
     // TODOS OS METODOS DE ALTERAÇÃO E EXIBIÇÃO DE CLIENTES E PRODUTOS DEVEM VIR NESSA CLASSE
     
 }
